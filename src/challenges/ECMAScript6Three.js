3e588414dcf4c03fd15042313e3b9e9e606b4621
tamara-munoz <tami.skt@gmail.com>
2021-05-30 23:29:32 -0400

    feat: objects

--- src/challenges/ECMAScript6Three.js
+++ src/challenges/ECMAScript6Three.js
@@ -1,3 +1,15 @@
+// Arrays
+
 let array_one = [ 1, 2, 3, 4]
 let array_two = [ 5, 6, 7, 8]
 let joined_array = array_one.concat(array_two)
+
+
+// Objects
+
+let arr = []
+
+let object_one = { attr: 1, attr_two: 2 }
+let object_two = { attr_three: 3, attr_four: 4 }
+arr.push(object_one)
+arr.push(object_two)
